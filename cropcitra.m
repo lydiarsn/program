ca39e1b8a1a4bec8229f9b05d20d5f69d862017d
lydiarsn <lydia.radita28@gmail.com>
2020-04-16 21:08:42 +0700

    potong citra

---
+++ cropcitra.m
@@ -0,0 +1,16 @@
+%dimensi crop yang diinginkan
+close all;
+clear all;
+clc;
+
+load datasampel
+tg=6000
+lb=2500
+fgabung=zeros(tg,lb,length(datasampel));
+for i=1; length (datasampel)
+    f=datasampel(i);
+    f=rgb2gray (f);
+    fc=potongcitra (f,tg,lb);
+    fgabung(:,:,i)=uint8(fc);
+    subplot(2,3,i);imshow(fc);
+end
