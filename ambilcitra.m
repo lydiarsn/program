5b0088a0b18cf19f1570de04480a4ebe9abb5cab
lydiarsn <lydia.radita28@gmail.com>
2020-04-16 21:05:30 +0700

    ambil citra dalam variabel

---
+++ ambilcitra.m
@@ -0,0 +1,12 @@
+close all;
+clear all;
+clc;
+
+i1=imread('F:\TATA\TUGAS AKHIR\DATA PROYEKSI\BMP\5.bmp');
+i2=imread('F:\TATA\TUGAS AKHIR\DATA PROYEKSI\BMP\10.bmp');
+i3=imread('F:\TATA\TUGAS AKHIR\DATA PROYEKSI\BMP\15.bmp');
+i4=imread('F:\TATA\TUGAS AKHIR\DATA PROYEKSI\BMP\20.bmp');
+i5=imread('F:\TATA\TUGAS AKHIR\DATA PROYEKSI\BMP\25.bmp');
+
+datasampel={i1,i2,i3,i4,i5};
+save=['datasampel','datasampel'];
